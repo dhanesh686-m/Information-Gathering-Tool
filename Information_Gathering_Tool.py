03d4289aae6795a0dcbc004a623b11bd7e9365fc
dhanesh686-m <157782918+dhanesh686-m@users.noreply.github.com>
2025-07-12 17:24:46 +0000

    Add files via upload

---
+++ Information_Gathering_Tool.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+import json
+import socket
+if len(sys.argv) <2:
+print("Usage: " + sys.argv[0] + "<url>")
+sys.exit(1)
+req requests.get("https://"+sys.argv[1])
+print("\n"+str(req.headers))
+gethostby socket.gethostbyname(sys.argv[1])
+print("\nThe IP address of "sys.argv[1]+" is: "+gethostby_ + "\")
+#ipinfo.io
+req_two requests.get("https://ipinfo.io/"+gethostby_+"/json")
+resp_json.loads(req_two.text)
+print("Location: "+resp_["loc"])
+print("Region: "+resp_["region"])
+print("City: "+resp_["city"])
+print("Country: "+resp_["country"])
