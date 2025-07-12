6c54e1167dda9daccd2f4cb8077772acfae4b3c1
dhanesh686-m <157782918+dhanesh686-m@users.noreply.github.com>
2025-07-12 23:44:10 +0530

    Information_Gathering_Tool.py

--- Information_Gathering_Tool.py
+++ Information_Gathering_Tool.py
@@ -3,15 +3,15 @@ import requests
 import json
 import socket
 if len(sys.argv) <2:
-print("Usage: " + sys.argv[0] + "<url>")
-sys.exit(1)
-req requests.get("https://"+sys.argv[1])
+	print("Usage: " + sys.argv[0] + "<url>")
+	sys.exit(1)
+req = requests.get("https://"+sys.argv[1])
 print("\n"+str(req.headers))
-gethostby socket.gethostbyname(sys.argv[1])
-print("\nThe IP address of "sys.argv[1]+" is: "+gethostby_ + "\")
+gethostby_ = socket.gethostbyname(sys.argv[1])
+print("\nThe IP address of " + sys.argv[1] + " is: " + gethostby_ + "\n")
 #ipinfo.io
-req_two requests.get("https://ipinfo.io/"+gethostby_+"/json")
-resp_json.loads(req_two.text)
+req_two = requests.get("https://ipinfo.io/"+gethostby_+"/json")
+resp_json.loads(req_two.text) # type: ignore
 print("Location: "+resp_["loc"])
 print("Region: "+resp_["region"])
 print("City: "+resp_["city"])
